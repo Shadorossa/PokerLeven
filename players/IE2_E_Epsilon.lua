60a4a89d76e4fb982ad49a18f53953829cc23edc
Shadorossa <ignasingalcorrepersonal@gmail.com>
2026-03-19 14:07:15 +0100

    Player

--- players/IE2_E_Epsilon.lua
+++ players/IE2_E_Epsilon.lua
@@ -3,9 +3,19 @@ local Dvalin = {
   name = "Dvalin",
   pos = { x = 4, y = 0 },
   soul_pos = { x = 4, y = 1 },
-  config = { extra = {} },
+  config = { extra = { pposition = "GK", chip_mod = 150, barriers_added = 1, xmult_mod = 0.5, barriers_consumed = 1 } },
   loc_vars = function(self, info_queue, center)
-    return {}
+      local is_gk = center.ability.extra.pposition == "GK"
+      local barriers = G.GAME.current_round and G.GAME.current_round.barriers or 0
+      local current_xmult = 1 + (barriers * center.ability.extra.xmult_mod)
+      return { vars = { 
+          center.ability.extra.chip_mod, 
+          center.ability.extra.barriers_added, 
+          center.ability.extra.xmult_mod, 
+          center.ability.extra.barriers_consumed,
+          current_xmult,
+          is_gk and "GK" or "FW"
+      } }
   end,
   rarity = "ina_top", -- Destacado
   pools = { ["Epsilon"] = true },
@@ -17,6 +27,36 @@ local Dvalin = {
   techtype = C.UPGRADES.Number,
   blueprint_compat = true,
   calculate = function(self, card, context)
+    if context.joker_main and context.scoring_hand then
+        if card.ability.extra.pposition == "GK" then
+            Pokerleven.ease_barriers(card.ability.extra.barriers_added)
+            return {
+                message = localize{type='variable',key='a_chips',vars={card.ability.extra.chip_mod}},
+                chip_mod = card.ability.extra.chip_mod,
+                colour = G.C.CHIPS
+            }
+        elseif card.ability.extra.pposition == "FW" then
+            local barriers = G.GAME.current_round.barriers or 0
+            local cost = card.ability.extra.barriers_consumed
+            if barriers >= cost then
+                Pokerleven.ease_barriers(-cost)
+                local xmult = 1 + (barriers * card.ability.extra.xmult_mod)
+                return {
+                    message = localize{type='variable',key='a_xmult',vars={xmult}},
+                    Xmult_mod = xmult,
+                    colour = G.C.MULT
+                }
+            end
+        end
+    end
+  end,
+  update = function(self, card, dt)
+      if G.STAGE == G.STAGES.RUN and card.area == G.jokers then
+          local target_pos = Pokerleven.is_in_left_half(card) and "GK" or "FW"
+          if card.ability.extra.pposition ~= target_pos then
+             apply_property_sticker(card, target_pos, "position")
+          end
+      end
   end
 }
 
